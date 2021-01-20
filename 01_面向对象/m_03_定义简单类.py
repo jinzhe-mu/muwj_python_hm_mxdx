7b60720b2c86dce30733b43f438b64b5d29da879
jinzhe-mu <1421827450@qq.com>
2021-01-20 14:24:33 +0800

    类的定义，第一个简单的面向对象

---
+++ 01_面向对象/m_03_定义简单类.py
@@ -0,0 +1,45 @@
+"""
+定义简单的类（只包含方法）
+面向对象 是 更大 的 封装，在 一个类中 封装 多个方法，这样 通过这个类创建出来的对象，就可以直接调用这些方法了！
+语法格式：
+class 类名:
+
+    def 方法1(self, 参数列表):
+        pass
+
+    def 方法2(self, 参数列表):
+        pass
+
+当一个类定义完成之后，要使用这个类来创建对象，语法格式如下：
+对象变量 = 类名()
+"""
+"""
+需求
+
+小猫 爱 吃 鱼，小猫 在 喝 牛奶
+分析
+
+定义一个猫类 Cat
+定义两个方法 eat 和 drink
+按照需求 —— 不需要定义属性
+"""
+
+
+class Cat:
+    """这是一个猫类"""
+
+    def eat(self, color):
+
+        print(format(color), "猫爱吃鱼")
+
+    def drink(self, color):
+
+        self.eat(color)
+        print(format(color), "小猫再喝牛奶")
+
+
+# 创建猫对象
+tom = Cat()
+tom.drink("黑色")
+tom.eat("小白")
+
