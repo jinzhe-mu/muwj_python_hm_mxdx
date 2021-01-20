516e97c49a6d676428911f94241437c3380736a4
jinzhe-mu <1421827450@qq.com>
2021-01-20 17:12:33 +0800

    类外部属性简单设置（但不推荐），设置应该在类里面

---
+++ 01_面向对象/m_05_设置对象属性.py
@@ -0,0 +1,37 @@
+class Cat:
+    """这是一个猫类"""
+
+    def eat(self, color):
+
+        print(format(color), "猫爱吃鱼")
+
+    def drink(self, color):
+
+        self.eat(color)
+        print(format(color), "小猫在喝牛奶")
+
+
+# 创建第一个猫对象
+tom = Cat()
+
+"""
+在 Python 中，要 给对象设置属性，非常的容易，但是不推荐使用
+因为：对象属性的封装应该封装在类的内部
+只需要在 类的外部的代码 中直接通过 . 设置一个属性即可
+注意：这种方式虽然简单，但是不推荐使用！
+"""
+# 设置对象属性：可以用 .属性名  利用复制语句就可以了
+tom.name = "Tom "
+
+print(tom.name)
+tom.eat("1.1 小白")
+tom.drink("1.2 黑色")
+
+
+# 创建第二个猫对象
+mak = Cat()
+mak.eat("2.1 花花")
+mak.drink("2.2 灰点")
+print(mak)
+
+
