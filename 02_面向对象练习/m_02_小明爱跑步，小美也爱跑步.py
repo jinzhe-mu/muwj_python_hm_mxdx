bd54fcabfd5a7887a35b6e878728740b55375385
jinzhe-mu <1421827450@qq.com>
2021-01-26 16:15:20 +0800

    多个对象属性之间互不干扰（调用同一个类）

--- 02_面向对象练习/m_02_小明爱跑步，小美也爱跑步.py
+++ 02_面向对象练习/m_02_小明爱跑步，小美也爱跑步.py
@@ -1,6 +1,9 @@
 """
-需求
+提示：
+在 对象的方法内部，是可以 直接访问对象的属性 的
+同一个类 创建的 多个对象 之间，属性 互不干扰！
 
+需求：
 小明 和 小美 都爱跑步
 小明 体重 75.0 公斤
 小美 体重 45.0 公斤
@@ -32,20 +35,31 @@ class Person:
         self.weight += 1
 
 
-xiaoming = Person("小明", 59)
+xiaoming = Person("小明", 75)
 xiaoming.run()
 print(xiaoming)
 xiaoming.eat()
 print(xiaoming)
 
-del xiaoming
 
-xiaomei = Person("小美", 48)
+xiaomei = Person("小美", 45)
 xiaomei.run()
 print(xiaomei)
 xiaomei.eat()
 print(xiaomei)
+print(xiaoming)  # xiaomei的调用不会影响xiaoming的调用结果
 
-del xiaomei
 
+"""
+结果：
+小明爱跑步,跑步锻炼身体
+我的名字叫小明，我的体重是74.5公斤
+小明爱吃零食，容易变胖
+我的名字叫小明，我的体重是75.5公斤
+小美爱跑步,跑步锻炼身体
+我的名字叫小美，我的体重是44.5公斤
+小美爱吃零食，容易变胖
+我的名字叫小美，我的体重是45.5公斤
+我的名字叫小明，我的体重是75.5公斤
+"""
 
